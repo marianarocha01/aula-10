6448fef8abe6b7629f8df904dfdd122c34efe817
marianarocha01 <146870657+marianarocha01@users.noreply.github.com>
2023-11-27 13:25:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let name = "Mariana";
+console.log(name)
+let number = 19;
+console.log(number)
+let number_multiplicado = number * 7;
+console.log(number_multiplicado)
+let amigos = ["carol", "gonçalo", "maria"];
+let numbers = [1, 2, 3, 4, 5];
+console.log(amigos[0]);
+// carol
+console.log(amigos[1]);
+// gonçalo
+console.log(amigos[2]);
+// maria
+let person = {
+    name: "Mariana",
+    age: 19,
+    Student: "verdadeiro"
+  };
+  console.log(person.name);
+  // Mariana
+  console.log(person.age);
+  // 19
+  console.log(person.Student);
+  // student
+  let person2 = {
+    name: "Carol",
+    age: 18,
+    Student: "verdadeiro"
+  };
+  console.log(person2.name);
+  // Carol
+  console.log(person2.age);
+  // 18
+  console.log(person2.Student);
+  // student
+  let person3  = {
+    name: "gonçalo",
+    age: 19,
+    Student: "verdadeiro"
+  };
+  console.log(person3.name);
+  // gonçalo
+  console.log(person3.age);
+  // 19
+  console.log(person3.Student);
+  // studant
+  let person4 = {
+    name: "maria",
+    age: 18,
+    Student: "verdadeiro"
+  };
+  console.log(person4.name);
+  // Maria
+  console.log(person4.age);
+  // 18
+  console.log(person4.Student);
+  // student
